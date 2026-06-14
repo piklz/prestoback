7a235aede479a6c08c90ced3ca5c85bcc0f4838e
piklz <pixelpiklz@gmail.com>
2026-06-14 21:55:32 +0100

    new file

---
+++ internal/api/disk.go
@@ -0,0 +1,19 @@
+package api
+
+import "syscall"
+
+type diskStat struct {
+	free  uint64
+	total uint64
+}
+
+func diskUsage(path string) (diskStat, error) {
+	var s syscall.Statfs_t
+	if err := syscall.Statfs(path, &s); err != nil {
+		return diskStat{}, err
+	}
+	return diskStat{
+		free:  s.Bavail * uint64(s.Bsize),
+		total: s.Blocks * uint64(s.Bsize),
+	}, nil
+}
